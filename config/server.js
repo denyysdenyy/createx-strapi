1373034d265993544ab458d053c79486f568ba27
denyys <denyysitbelikov@gmail.com>
2023-07-11 00:56:50 +0300

    third commit

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'dpg-cim6oo15rnuvtgtcmb0g-a.frankfurt-postgres.render.com'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
