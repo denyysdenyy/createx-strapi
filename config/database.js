4d446b2806fc4c4dd951a56852fefa5555a7d55d
denyys <denyysitbelikov@gmail.com>
2023-07-11 00:16:58 +0300

    second commit

--- config/database.js
+++ config/database.js
@@ -4,6 +4,20 @@ module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
+
+    mysql3:{
+      connection:{
+        client:'postgres',
+        connection:{
+          host: env('DATABASE_HOST', ),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME' ),
+          password: env('DATABASE_PASSWORD'),
+          ssl: env.bool('DATABASE_SSL', true)
+        }
+      }
+    },
     mysql: {
       connection: {
         connectionString: env('DATABASE_URL'),
@@ -26,6 +40,7 @@ module.exports = ({ env }) => {
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
+   
     mysql2: {
       connection: {
         host: env('DATABASE_HOST', 'localhost'),
