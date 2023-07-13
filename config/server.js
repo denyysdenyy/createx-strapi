02769f0c752cea80744a17d40d1435940e778d48
denyys <denyysitbelikov@gmail.com>
2023-07-13 14:32:51 +0300

    strapi update

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', 'dpg-cim6oo15rnuvtgtcmb0g-a.frankfurt-postgres.render.com'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 2005),
   app: {
     keys: env.array('APP_KEYS'),
   },
