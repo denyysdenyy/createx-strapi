dfa648d61e8945f8828f03a960934cf9f70576f2
denyys <denyysitbelikov@gmail.com>
2023-07-18 02:52:09 +0300

    fix problems

--- config/database.js
+++ config/database.js
@@ -5,19 +5,19 @@ module.exports = ({ env }) => {
 
   const connections = {
 
-    mysql3:{
-      connection:{
-        client:'postgres',
-        connection:{
-          host: env('DATABASE_HOST', ),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME' ),
-          password: env('DATABASE_PASSWORD'),
-          ssl: env.bool('DATABASE_SSL', true)
-        }
-      }
-    },
+    // mysql3:{
+    //   connection:{
+    //     client:'postgres',
+    //     connection:{
+    //       host: env('DATABASE_HOST', ),
+    //       port: env.int('DATABASE_PORT'),
+    //       database: env('DATABASE_NAME'),
+    //       user: env('DATABASE_USERNAME' ),
+    //       password: env('DATABASE_PASSWORD'),
+    //       ssl: env.bool('DATABASE_SSL', true)
+    //     }
+    //   }
+    // },
     mysql: {
       connection: {
         connectionString: env('DATABASE_URL'),
