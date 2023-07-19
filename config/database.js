e9249140d0d17beb4db3be517b2835469b2ac100
denyys <denyysitbelikov@gmail.com>
2023-07-19 03:06:20 +0300

    first commit

--- config/database.js
+++ config/database.js
@@ -1,23 +1,29 @@
 const path = require('path');
 
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+      },
+    },
+    debug: false,
+  },
+});
+
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
 
-    mysql3:{
-      connection:{
-        client:'postgres',
-        connection:{
-          host: env('DATABASE_HOST', ),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME' ),
-          password: env('DATABASE_PASSWORD'),
-          ssl: env.bool('DATABASE_SSL', true)
-        }
-      }
-    },
+   
     mysql: {
       connection: {
         connectionString: env('DATABASE_URL'),
--- config/server.js
+++ config/server.js
@@ -1,10 +1,18 @@
 module.exports = ({ env }) => ({
   host: env('HOST', 'dpg-cim6oo15rnuvtgtcmb0g-a.frankfurt-postgres.render.com'),
-  port: env.int('PORT', 3000),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
+
+module.exports = ({ env }) => ({
+  proxy: true,
+  url: env('APP_URL'), // Sets the public URL of the application.
+  app: { 
+    keys: env.array('APP_KEYS')
   },
 });
